0982499c7d384bd02a7ba2929036a3b20a147e51
Wil-g2 <wil-g2@hotmail.com>
2020-07-22 20:37:30 -0300

    chore: added global styles of styled-components

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   *{
@@ -7,7 +7,7 @@ export default createGlobalStyle`
     outline: 0;
     box-sizing: border-box;
   }
-  
+
   html, body, #root {
     min-height: 100%;
   }
@@ -25,5 +25,19 @@ export default createGlobalStyle`
 
   button {
     cursor: pointer;
+  }
+
+  :root {
+    --primary: #000;
+    --secondary: #15181C;
+    --search: #202327;
+    --white: #D9D9D9;
+    --gray: #7A7A7A;
+    --outline: #2F3336;
+    --retweet: #00C06B;
+    --error: #E8265E;
+    --twitter: #33A1F2;
+    --twitter-dark-hover: #011017;
+    --twitter-light-hover: #2C8ED6;
   }
 `;
